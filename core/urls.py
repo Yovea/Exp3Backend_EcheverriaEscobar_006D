e2c93dd9fd72ce311a705c6a7bf3f2e9ac83c3f8
Yovea <66392129+Yovea@users.noreply.github.com>
2021-06-20 18:55:23 -0400

    Navbar pasada a django, paginas tecnicamente funcionando como antes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, whyarehere, cutestpets, exitscam, dummylogin ,dummydata, succes
 
 urlpatterns = [
     path('', home,name="home"),
+    path('Quienes-somos', whyarehere, name='Quienessomos'),
+    path('Galeria', cutestpets, name='Galeria'),
+    path('Compra', exitscam, name='Compra'),
+    path('Ingreso', dummylogin, name='Form1'),
+    path('Registro', dummydata, name='Form2'),
+    path('Fin', succes, name='End'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-
     return render(request, 'core/home.html')
+
+def whyarehere(request):
+    return render(request, 'core/whyarehere.html')
+
+def cutestpets(request):
+    return render(request, 'core/cutestpets.html')
+
+def exitscam(request):
+    return render(request, 'core/exitscam.html')
+
+def dummylogin(request):
+    return render(request, 'core/dummylogin.html')
+
+def dummydata(request):
+    return render(request, 'core/dummydata.html')
+
+def succes(request):
+    return render(request, 'core/succes.html')
