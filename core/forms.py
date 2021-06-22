f8038bafef95e3d0fa3591761e7d12d22853096b
Yovea <66392129+Yovea@users.noreply.github.com>
2021-06-22 19:42:40 -0400

    Cosa

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,6 @@
 from django import forms
-from django.forms import ModelForm, widgets
-from django.forms.models import ModelChoiceField
-from django.forms.widgets import widgets
-from . models import Formulario
+from django.forms import ModelForm
+from .models import Formulario
 
 #hell.
 
@@ -11,47 +9,4 @@ class FormularioForm(ModelForm):
     class Meta:
         model = Formulario 
         fields =['rutFormulario', 'nombreFormulario', 'mailFormulario', 'passFormulario', 'celnumFormulario']
-        labels ={
-            'rutFormulario': 'Rut',
-            'nombreFormulario': 'Nombre',
-            'mailFormulario': 'Mail',
-            'passFormulario': 'Password',
-            'celnumFormulario': 'Celular',
-        }
-        widgets={
-            'rutFormulario': forms.TextInput(
-                attrs={
-                'class': 'form-control',
-                'placeholder': 'Ingresar rut',
-                'id': 'rut'
-                }
-            ),
-            'nombreFormulario': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Ingresar Nombre',
-                    'id': 'Nombre'
-                }
-            ),
-            'mailFormulario': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Ingresar Mail',
-                    'id': 'Mail'
-                }
-            ),
-            'passFormulario': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Ingresar Contraseña',
-                    'id': 'Pass'
-                }
-            ),
-            'celnumFormulario': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Ingresar numero telefonico',
-                    'id': 'Cel'
-                }
-            )
-        }
+ 
